e53c90706081469606989ca9679627cf92ce0044
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-11-28 11:56:41 +0530

    Update tshirts.c
    added boundary value check test cases

--- tshirts.c
+++ tshirts.c
@@ -18,5 +18,10 @@ int main() {
     assert(size(40) == 'M');
     assert(size(43) == 'L');
     printf("All is well (maybe!)\n");
+    //newly added tests
+    assert(size(38) == 'S'); //Test to check the boundary value 38 for "s" size
+    assert(size(38) == 'M'); //Test to check the boundary value 38 for "m" size
+    assert(size(42) == 'M'); //Test to check the boundary value 42 for "m" size
+    assert(size(42) == 'L'); //Test to check the boundary value 42 for "l" size
     return 0;
 }
