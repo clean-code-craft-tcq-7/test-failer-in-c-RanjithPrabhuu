0e917a3f83fc3dd24560dfab4bbdf2af7aa3030a
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-11-29 11:56:45 +0530

    Update misaligned.c
    updated the test cases

--- misaligned.c
+++ misaligned.c
@@ -33,11 +33,11 @@ int main()
     assert(strcmp(test_colorPairData[0].majorColor, majorColor[0]) == 0);
     assert(strcmp(test_colorPairData[0].minorColor, minorColor[0]) == 0);
     
-    assert(test_colorPairData[1].colorPairNumber == 2);
+    assert(test_colorPairData[1].colorPairNumber == 1);
     assert(strcmp(test_colorPairData[1].majorColor, majorColor[0]) == 0);
     assert(strcmp(test_colorPairData[1].minorColor, minorColor[1]) == 0);
     
-    assert(test_colorPairData[2].colorPairNumber == 3);
+    assert(test_colorPairData[2].colorPairNumber == 2);
     assert(strcmp(test_colorPairData[2].majorColor, majorColor[0]) == 0);
     assert(strcmp(test_colorPairData[2].minorColor, minorColor[2]) == 0);
     
@@ -45,11 +45,10 @@ int main()
     assert(strcmp(test_colorPairData[3].majorColor, majorColor[0]) == 0);
     assert(strcmp(test_colorPairData[3].minorColor, minorColor[3]) == 0);
     
-    assert(test_colorPairData[4].colorPairNumber == 3);
+    assert(test_colorPairData[4].colorPairNumber == 4);
     assert(strcmp(test_colorPairData[4].majorColor, majorColor[0]) == 0);
     assert(strcmp(test_colorPairData[4].minorColor, minorColor[4]) == 0);
-    
-    
+      
     printf("All is well (maybe!)\n");
     return 0;
 }
