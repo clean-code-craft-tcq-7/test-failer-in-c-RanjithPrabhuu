9679ee0d95ab0b4962a004d6207b37d2f2024c2f
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-12-06 10:42:26 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -42,27 +42,27 @@ int main()
     
     // major color "white" is taken for test case validation
     //test case to check the pair number 1 and its major and minor color. 
-    assert(test_colorPairData[0].colorPairNumber == 0); //This will pass
+    assert(test_colorPairData[0].colorPairNumber == 1); //This will pass
     assert(strcmp(test_colorPairData[0].majorColor, majorColor[0]) == 0); //This will pass
     assert(strcmp(test_colorPairData[0].minorColor, minorColor[0]) == 0); //This will pass
     
     //test case to check the pair number 2 and its major and minor color.
-    assert(test_colorPairData[1].colorPairNumber == 1); //This will pass
+    assert(test_colorPairData[1].colorPairNumber == 2); //This will pass
     assert(strcmp(test_colorPairData[1].majorColor, majorColor[0]) == 0); //This will pass
     assert(strcmp(test_colorPairData[1].minorColor, minorColor[1]) == 0); //This will fail since the minorColor[i] will be same for all the 5 pairs of a major color
     
     //test case to check the pair number 3 and its major and minor color.
-    assert(test_colorPairData[2].colorPairNumber == 2); //This will pass
+    assert(test_colorPairData[2].colorPairNumber == 3); //This will pass
     assert(strcmp(test_colorPairData[2].majorColor, majorColor[0]) == 0); //This will pass
     assert(strcmp(test_colorPairData[2].minorColor, minorColor[2]) == 0); //This will fail since the minorColor[i] will be same for all the 5 pairs of single major color
     
     //test case to check the pair number 4 and its major and minor color.
-    assert(test_colorPairData[3].colorPairNumber == 3); //This will pass
+    assert(test_colorPairData[3].colorPairNumber == 4); //This will pass
     assert(strcmp(test_colorPairData[3].majorColor, majorColor[0]) == 0); //This will pass
     assert(strcmp(test_colorPairData[3].minorColor, minorColor[3]) == 0); //This will fail since the minorColor[i] will be same for all the 5 pairs of a major color
     
     //test case to check the pair number 5 and its major and minor color.This will fail
-    assert(test_colorPairData[4].colorPairNumber == 4); //This will pass
+    assert(test_colorPairData[4].colorPairNumber == 5); //This will pass
     assert(strcmp(test_colorPairData[4].majorColor, majorColor[0]) == 0); //This will pass
     assert(strcmp(test_colorPairData[4].minorColor, minorColor[4]) == 0); //This will fail since the minorColor[i] will be same for all the 5 pairs of a major color
       
