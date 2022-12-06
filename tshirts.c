7976785ef5e8824f55390beb4268357d1a466c61
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-12-06 08:13:26 +0530

    Update tshirts.c
    updated the code

--- tshirts.c
+++ tshirts.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 #include <assert.h>
 
-char size(int cms) {
+char size(int cms) 
+{
     char sizeName = '\0';
-    if(cms < 38) {
+    if(cms < 38) 
+    {
         sizeName = 'S';
-    } else if(cms >= 38 && cms < 42) {
+    } 
+    else if(cms >= 38 && cms < 42) 
+    {
         sizeName = 'M';
-    } else if(cms >=  42) {
+    } 
+    else if(cms >=  42) 
+    {
         sizeName = 'L';
     }
     return sizeName;
 }
 
-int main() {
+int main() 
+{
     assert(size(37) == 'S');
     assert(size(40) == 'M');
     assert(size(43) == 'L');
