554e71c45f04bd69e450fd74e613306a5fae617e
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-12-06 07:02:46 +0530

    Update tshirts.c
    update the code

--- tshirts.c
+++ tshirts.c
@@ -5,9 +5,9 @@ char size(int cms) {
     char sizeName = '\0';
     if(cms < 38) {
         sizeName = 'S';
-    } else if(cms >= 38 && cms <= 42) {
+    } else if(cms >= 38 && cms < 42) {
         sizeName = 'M';
-    } else if(cms > 42) {
+    } else if(cms >=  42) {
         sizeName = 'L';
     }
     return sizeName;
@@ -18,12 +18,12 @@ int main() {
     assert(size(40) == 'M');
     assert(size(43) == 'L');
     
-    // Assuming size 38 is for "m" and size 42 is also "l". With this understanding s < 38 , m >= 38 and < 42 , l >= 42
-    //assert(size(35) == 'S');
+    // Assuming size 38 is for "m" and size 42 is for "l". With this understanding s < 38 , m >= 38 and < 42 , l >= 42
+    assert(size(35) == 'S');
     assert(size(38) == 'M');
-    //assert(size(40) == 'M');
-    assert(size(42) == 'M');
-    //assert(size(44) == 'L');
+    assert(size(40) == 'M');
+    assert(size(42) == 'L');
+    assert(size(44) == 'L');
     
     printf("All is well (maybe!)\n");
     return 0;
