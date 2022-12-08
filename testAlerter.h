75b376f0fefff0b2b507198efcfbe74a77eb3ef6
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-12-08 08:46:29 +0530

    Create testAlerter.h
    created a test.h file for alerter function

---
+++ testAlerter.h
@@ -0,0 +1,5 @@
+// function declaration of network test stub function
+int networkAlertStub(float celcius);
+
+// extern of variable which returns the network alert status
+extern int testNetworkAlertStatus;
