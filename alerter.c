213d8b7ca994c6cd3c5cc8c482e660862ddbb135
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-12-08 08:49:42 +0530

    Update alerter.c
    removed test functions

--- alerter.c
+++ alerter.c
@@ -1,21 +1,9 @@
 #include <stdio.h>
 #include <assert.h>
-#include "config.h"
+#include "testAlerter.h"
 
 // flag to return the failure count
 int alertFailureCount = 0;
-
-//flag to return the alert status for test cases
-int test_AlertStatusOfNetwork = 0;
-
-int networkAlertStub(float celcius) 
-{
-    printf("ALERT: Temperature is %.1f celcius.\n", celcius);
-    // Return 200 for ok
-    // Return 500 for not-ok
-    // stub always succeeds and returns 200
-    return test_AlertStatusOfNetwork;
-}
 
 void alertInCelcius(float farenheit,int(*networkAlert)(float), float threshold) 
 {
